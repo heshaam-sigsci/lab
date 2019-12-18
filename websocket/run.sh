e1a4103e77f39c42aec919057d8086b549e46cb4
Heshaam Attar <heshaam@signalsciences.com>
2019-12-18 10:27:18 -0800

    custom websocket example

---
+++ websocket/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+npm install ws
+
+cat << EOS > server.js
+console.log("Server started");
+var Msg = '';
+var WebSocketServer = require('ws').Server
+    , wss = new WebSocketServer({port: 8010});
+    wss.on('connection', function(ws) {
+        ws.on('message', function(message) {
+        console.log('Received from client: %s', message);
+        ws.send('Server received from client: ' + message);
+    });
+ });
+EOS
+
+node server.js
